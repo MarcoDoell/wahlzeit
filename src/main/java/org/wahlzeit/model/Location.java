f9a9a9bdbd129b25d553a50daab85eb85586efd5
marco döll <marco.doell@hotmail.com>
2020-11-09 20:24:17 +0100

    added coordinate property in location class

--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -1,4 +1,15 @@
 package org.wahlzeit.model;
 
 public class Location {
+
+    /**
+     * Coordinates of the location
+     */
+    public Coordinate coordinate;
+
+    public Location(Coordinate coordinate) {
+        if(coordinate == null)
+            throw new IllegalArgumentException("Coordinate is not allowed to be null");
+        this.coordinate = coordinate;
+    }
 }
