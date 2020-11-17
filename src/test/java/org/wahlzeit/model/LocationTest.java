26899c46f9f4f08b5a74fce52e1ac630a61ac21f
marco döll <marco.doell@hotmail.com>
2020-11-17 17:11:24 +0100

    added locationtest class

---
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -0,0 +1,39 @@
+package org.wahlzeit.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Test cases for the Location class.
+ */
+public class LocationTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithNullParameter() {
+        //Act
+        Location location = new Location(null);
+    }
+
+    @Test
+    public void testConstructorWorks() {
+        //Act & Arrange
+        Location location = new Location(new Coordinate(0,0,0));
+
+        //Assert
+        assertNotNull(location);
+        assertNotNull(location.getCoordinate());
+    }
+
+    @Test
+    public void testConstructorSavesRightParametersOfCoordinateClass() {
+        //Act & Arrange
+        Location location = new Location(new Coordinate(0,0,0));
+
+        //Assert
+        assertEquals(location.getCoordinate().getX(),0,0);
+        assertEquals(location.getCoordinate().getY(),0,0);
+        assertEquals(location.getCoordinate().getZ(),0,0);
+    }
+}
