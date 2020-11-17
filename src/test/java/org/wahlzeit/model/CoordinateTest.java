93861c93ccfdf3aac9234818dc24320e00d9a0f3
marco döll <marco.doell@hotmail.com>
2020-11-17 17:06:29 +0100

    added further coordinate tests

--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -9,25 +9,78 @@ import static org.junit.Assert.assertEquals;
  */
 public class CoordinateTest {
 
-
     @Test
-    public void testGetDistanceWith100() {
+    public void testGetDistanceWith200to100() {
         //Act
-        Coordinate coord = new Coordinate(1,0,0);
+        Coordinate coord1 = new Coordinate(2,0,0);
+        Coordinate coord2 = new Coordinate(1,0,0);
 
         //Arrange
-        double result = coord.getDistance(coord);
+        double result = coord1.getDistance(coord2);
 
         //Assert
-        assertEquals(0,result,0.1);
+        assertEquals(1,result,0.1);
     }
 
     @Test(expected = NullPointerException.class)
     public void testGetDistanceThrowsException() {
         //Act
-        Coordinate coord = new Coordinate(0,0,0);
+        Coordinate coord1 = new Coordinate(0,0,0);
+        Coordinate coord2 = null;
 
         //Arrange
-        coord.getDistance(null);
+        coord1.getDistance(coord2);
+    }
+
+    @Test()
+    public void testisEqualworks() {
+        //Act
+        Coordinate coord1 = new Coordinate(0,0,0);
+        Coordinate coord2 = new Coordinate(0,0,0);
+
+        //Arrange
+        boolean result = coord1.equals(coord2);
+
+        //Assert
+        assertEquals(result,true);
+    }
+
+    @Test()
+    public void testisEqualDoesntWorks() {
+        //Act
+        Coordinate coord1 = new Coordinate(0,0,0);
+        Coordinate coord2 = new Coordinate(1,0,0);
+
+        //Arrange
+        boolean result = coord1.equals(coord2);
+
+        //Assert
+        assertEquals(result,false);
+    }
+
+    @Test()
+    public void testEqualsWorks() {
+        //Act
+        Coordinate coord1 = new Coordinate(0,0,0);
+        Coordinate coord2 = new Coordinate(0,0,0);
+
+        //Arrange
+        boolean result = coord1.equals(coord2);
+
+        //Assert
+        assertEquals(result,true);
+    }
+
+    @Test()
+    public void testEqualsDoesntWorks() {
+        //Act
+        Coordinate coord1 = new Coordinate(0,0,0);
+        Coordinate coord2 = new Coordinate(1,0,0);
+
+        //Arrange
+        boolean result = coord1.equals(coord2);
+
+        //Assert
+        assertEquals(result,false);
     }
 }
