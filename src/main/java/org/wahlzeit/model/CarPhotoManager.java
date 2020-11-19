869d38ec61351a93e0a7e5e01dc4617ddb1bc10d
marco döll <marco.doell@hotmail.com>
2020-11-19 14:15:56 +0100

    first implementation of using my photo class in the photomanager

--- src/main/java/org/wahlzeit/model/CarPhotoManager.java
+++ src/main/java/org/wahlzeit/model/CarPhotoManager.java
@@ -1,8 +1,28 @@
 package org.wahlzeit.model;
 
+import java.io.File;
+
 public class CarPhotoManager extends PhotoManager {
 
     public CarPhotoManager() {
         super();
+    }
+
+    @Override
+    public CarPhoto createPhoto(File file) throws Exception {
+        PhotoId id = PhotoId.getNextId();
+        CarPhoto result = (CarPhoto) PhotoUtil.createPhoto(file, id);
+        addPhoto(result);
+        return result;
+    }
+
+    @Override
+    protected CarPhoto getPhotoFromFilter(PhotoFilter filter) {
+        return (CarPhoto) super.getPhotoFromFilter(filter);
+    }
+
+    @Override
+    protected CarPhoto doGetPhotoFromId(PhotoId id) {
+        return (CarPhoto) super.doGetPhotoFromId(id);
     }
 }
