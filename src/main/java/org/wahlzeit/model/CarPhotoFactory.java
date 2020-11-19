50f5c811e479bae6a42539d2fc5c4b86c42091e9
marco döll <marco.doell@hotmail.com>
2020-11-19 14:18:15 +0100

    first implementation of carphotofactory overrided the methods to make sure that my carphotoclass is beeing used

--- src/main/java/org/wahlzeit/model/CarPhotoFactory.java
+++ src/main/java/org/wahlzeit/model/CarPhotoFactory.java
@@ -1,8 +1,26 @@
 package org.wahlzeit.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CarPhotoFactory extends PhotoFactory {
 
     public CarPhotoFactory() {
         super();
+    }
+
+    @Override
+    public CarPhoto createPhoto() {
+        return new CarPhoto();
+    }
+
+    @Override
+    public CarPhoto createPhoto(PhotoId id) {
+        return new CarPhoto(id);
+    }
+
+    @Override
+    public CarPhoto createPhoto(ResultSet rs) throws SQLException {
+        return new CarPhoto(rs);
     }
 }
