73d3bd5973928d42cf0feb9620f39250a204f077
marco döll <marco.doell@hotmail.com>
2020-11-22 12:12:11 +0100

    initialize carphotofactory in startup

--- src/main/java/org/wahlzeit/model/CarPhoto.java
+++ src/main/java/org/wahlzeit/model/CarPhoto.java
@@ -33,9 +33,7 @@ public class CarPhoto extends Photo {
         this.car.setBrand(rset.getString("brand"));
     }
 
-    /**
-     *
-     */
+    @Override
     public void writeOn(ResultSet rset) throws SQLException {
         super.writeOn(rset);
         rset.updateString("brand",this.car.getBrand());
