24b6c44b977f713b16eedc4003b32ec0718cb01a
marco döll <marco.doell@hotmail.com>
2020-11-28 15:10:04 +0100

    added test for photo

--- src/test/java/org/wahlzeit/model/PhotoTest.java
+++ src/test/java/org/wahlzeit/model/PhotoTest.java
@@ -3,6 +3,7 @@ package org.wahlzeit.model;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.wahlzeit.model.Coordinate.CartesianCoordinate;
+import org.wahlzeit.model.Coordinate.SphericCoordinate;
 import org.wahlzeit.services.EmailAddress;
 
 import java.net.MalformedURLException;
@@ -22,6 +23,7 @@ public class PhotoTest {
         Mockito.when(resultSet.getString("location")).thenReturn("1.0/2.0/3.0");
         Mockito.when(resultSet.getString("owner_email_address")).thenReturn("wahlzeit@wahlzeit.de");
         Mockito.when(resultSet.getString("owner_home_page")).thenReturn("http://www.wahlzeit.de");
+        Mockito.when(resultSet.getString("coordinateidentifier")).thenReturn("spheric");
 
         Photo photo = new Photo();
 
@@ -30,11 +32,12 @@ public class PhotoTest {
 
         //Assert
         verify(resultSet).getString("location");
+        verify(resultSet).getString("coordinateidentifier");
     }
 
 
     @Test
-    public void testWriteOn() throws SQLException, MalformedURLException {
+    public void testWriteOnWithCartesian() throws SQLException, MalformedURLException {
         //Arrange
         ResultSet resultSet = Mockito.mock(ResultSet.class);
 
@@ -48,6 +51,25 @@ public class PhotoTest {
 
         //Assert
         verify(resultSet).updateString("location",photo.location.toString());
+        verify(resultSet).updateString("coordinateidentifier","cartesian");
+    }
+
+    @Test
+    public void testWriteOnWithSpheric() throws SQLException, MalformedURLException {
+        //Arrange
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+
+        Photo photo = new Photo();
+        photo.setLocation(new Location(new SphericCoordinate(1.0,2.0,3.0)));
+        photo.setOwnerEmailAddress(EmailAddress.getFromString("wahlzeit@wahlzeit.de"));
+        photo.setOwnerHomePage(new URL("http://www.wahlzeit.de"));
+
+        //Act
+        photo.writeOn(resultSet);
+
+        //Assert
+        verify(resultSet).updateString("location",photo.location.toString());
+        verify(resultSet).updateString("coordinateidentifier","spheric");
 
     }
 }
